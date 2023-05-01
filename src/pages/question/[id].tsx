1aac4f0faf68721dc22e582e96b91af48473dab3
martwozniak <persky03@gmail.com>
2023-05-01 08:08:54 +0200

    seo: rich results tags

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import React, { useState } from 'react'
 import { api } from "../../utils/api";
+import Head from 'next/head';
 const ALPHABET = ["A","B","C", "D", "E", "F", "G", "H", "I", "J", "K"];
 
 export default function ShowSingleQuestion() {
@@ -26,7 +27,37 @@ export default function ShowSingleQuestion() {
     })
   }
 
+  const answerCount = singleQuestionQuery.data?.answers?.length;
+  const correctBody = singleQuestionQuery.data?.answers?.map((a) => {
+    if(a.isCorrect) {
+      return(a.body);
+    }
+  });
+
   return (
+    <>
+    <Head>
+      <title>{singleQuestionQuery?.data?.body}</title>
+      <meta name="description" content={singleQuestionQuery?.data?.body} />
+      <script type="application/ld+json" dangerouslySetInnerHTML={{__html:`
+    {
+    "@context": "https://schema.org",
+    "@type": "Question",
+    "name": "${singleQuestionQuery?.data?.body || ""}",
+    "upvoteCount": "196",
+    "text": "${singleQuestionQuery?.data?.body || ""}",
+    "answerCount": "${String(answerCount)}",
+    "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "${String(correctBody) || ""}",
+    },
+    "suggestedAnswer": {
+        "@type": "Answer",
+        "text": "${String(correctBody) || ""}",
+    }
+}
+`}}></script>
+    </Head>
     <div className='bg-slate-950 min-h-screen text-slate-200 px-4'>
       <div className='flex justify-center'>
           <div className='container border px-4 py-2 border-slate-800 rounded-xl py-6'>
@@ -43,11 +74,8 @@ export default function ShowSingleQuestion() {
                  findCorrect()          
               }}>Show correct answer</button>
           </div>
-
       </div>
-     
-
-
     </div>
+    </>
   )
 }
