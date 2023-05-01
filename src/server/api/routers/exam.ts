5cf920e1ceabb7c5e708d1baa3ba5b8ca9ebf1f0
martwozniak <persky03@gmail.com>
2023-05-01 22:59:22 +0200

    build: cleaning?

--- src/server/api/routers/exam.ts
+++ src/server/api/routers/exam.ts
@@ -22,18 +22,12 @@ export const examRouter = createTRPCRouter({
     return exam;
   }),
   getAllQuestionsAndAnswers: publicProcedure.query(({ ctx }) => {
-    // with answers 
-    //const questions = ctx.prisma.question.findMany();
+ 
     const questions = ctx.prisma.question.findMany({include: {answers: true}});
     return questions;
-  // return  questions;
-    //return ctx.prisma.question.findMany();
   }),
   getNQuestionsAndAnswers: publicProcedure.input(z.object({ numberOfQuestions: z.number()})).query(({ ctx,input }) => {
     const questionLimit  = Number(input.numberOfQuestions);
-    // const countAllQuestions = ctx.prisma.question.count();
-    // Randomize questions
-
     const questions = ctx.prisma.question.findMany({
       take: questionLimit,
       include: {answers: true}});
